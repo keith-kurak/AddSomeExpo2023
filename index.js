85a3d69a203ebf105b80166ca7bc74a5129d168d
Keith Kurak <keith.kurak@gmail.com>
2023-04-07 19:24:45 -0400

    Adopt Prebuild (#4)
    * get ready for prebuild
    
    * moved native projects to prebuild
    
    * updated min iOS/ Android versions

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import 'expo-dev-client';
-import {AppRegistry} from 'react-native';
+import {registerRootComponent} from 'expo';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(App);
